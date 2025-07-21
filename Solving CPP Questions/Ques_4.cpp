c172b56b604ebb1aec990136646258692b566714
Sujal Sharma <sujalsharma484@gmail.com>
2025-07-22 00:32:08 +0530

    Ques 4 added

---
+++ Solving CPP Questions/Ques_4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Q4. Write a Program to Swap Two Numbers.
+// Solution:
+
+int main() {
+	int num1, num2, num3;
+
+	num1 = 10;
+	num2 = 20;
+	cout << "Before swapping:" << endl;
+	cout << "num1: " << num1 << endl;
+	cout << "num2: " << num2 << endl << endl;
+
+	cout << "After swapping:" << endl;
+	num3 = num1; // num3 = 10
+	num1 = num2; // num1 = 20
+	num2 = num3; // num2 = 10
+
+	cout << "num1: " << num1 << endl; 
+	cout << "num2: " << num2 << endl; 
+}
