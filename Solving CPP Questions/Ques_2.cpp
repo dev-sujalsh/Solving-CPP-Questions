d2d7d85a5aa88db24f2dcf33ef803771a6140f9e
Sujal Sharma <sujalsharma484@gmail.com>
2025-07-22 00:12:56 +0530

    Ques 2 added

---
+++ Solving CPP Questions/Ques_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Q2. Write a Program to Read and Print Number Input from the User.
+// Solution:
+
+int main() {
+	int number;
+	cout << "Enter a number: ";
+	cin >> number; // Read input from the user
+
+	cout << "You entered: " << number << endl; // Print the number
+	return 0;
+}
