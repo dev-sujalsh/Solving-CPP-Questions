a962b3c4ea54662c4c31228a0e632ddf8bc575ff
Sujal Sharma <sujalsharma484@gmail.com>
2025-07-29 10:59:43 +0530

    file updation

---
+++ Solving CPP Questions/Ques_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// Q1. Write a Program to Print “Hello World” in the Console Screen.
+// Solution:
+
+int main() {
+	cout << "Hello, World!" << endl;
+	return 0;
+}
